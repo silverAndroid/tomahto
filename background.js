c939e32d426c9c655e91a93bbcfd45f5d245fa5f
Rushil Perera <pererarushil1940@gmail.com>
2017-06-24 12:10:14 -0400

    Closes tabs for unblocked domains

--- background.js
+++ background.js
@@ -7,18 +7,28 @@ function prefs() {
             'youtube.com',
             'twitter.com',
             'tumblr.com',
-            'pinterest.com',
-            'myspace.com',
-            'livejournal.com',
-            'digg.com',
-            'stumbleupon.com',
+            'instagram.com',
             'reddit.com',
-            'kongregate.com',
-            'newgrounds.com',
-            'addictinggames.com',
-            'hulu.com'
+            'amazon.ca'
         ]
     }
+}
+
+function init() {
+    var queryInfo = {
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function (tabs) {
+        // console.log(getUnblockedTabs(tabs));
+        closeTabs(getUnblockedTabs(tabs));
+    });
+}
+
+function getUnblockedTabs(tabs) {
+    return tabs.filter(function(tab) {
+        return !isDomainBlocked(tab.url);
+    });
 }
 
 function parseDomain(url) {
@@ -29,8 +39,31 @@ function domainsMatch(test, against) {
     return parseDomain(test) === parseDomain(against);
 }
 
+function isChromeURL(url) {
+    return url.startsWith('chrome://');
+}
+
 function isDomainBlocked(testSite) {
-    return PREFS.sites.some(function(site) {
+    return PREFS.sites.some(function (site) {
         return domainsMatch(site, testSite);
+    }) || isChromeURL(testSite);
+}
+
+function closeTabs(tabs) {
+    tabs.forEach(function(tab) {
+        closeTab(tab.id);
+    })    
+}
+
+function closeTab(tabID) {
+    chrome.tabs.remove(tabID, function () {
+        console.log('Kyle\'s dum');
     });
 }
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete') {
+    // do your things
+    init();
+  }
+});
